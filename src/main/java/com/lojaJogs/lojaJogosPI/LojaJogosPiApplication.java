be8b23c9c68079a8505d34b9657d678d59e9a67c
João Victor <jvna07@gmail.com>
2024-11-04 19:55:33 -0300

    Projeto inicial

---
+++ src/main/java/com/lojaJogs/lojaJogosPI/LojaJogosPiApplication.java
@@ -0,0 +1,13 @@
+package com.lojaJogs.lojaJogosPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaJogosPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaJogosPiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lojaJogs/lojaJogosPI/LojaJogosPiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojaJogs.lojaJogosPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaJogosPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
